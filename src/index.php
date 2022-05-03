08bcf2f973580dc30b187ace1cdffc3eba3f27c7
Ng Wing Tat, David <i@ngwingt.at>
2022-05-04 02:38:11 +0800

    ♻️ Apply get_header() & get_footer()

--- src/index.php
+++ src/index.php
@@ -1,9 +1,4 @@
-<html>
-  <head>
-    <title><?php echo get_bloginfo( 'name' ); ?></title>
-  </head>
-  <body>
-    <h1><?php echo get_bloginfo( 'name' ); ?></h1>
-    <footer>Copyright © <?php echo date('Y') ?>. All rights reserved.</footer>
-  </body>
-</html>
+<?php get_header() ?>
+  <h1><?php echo get_bloginfo( 'name' ); ?></h1>
+  <footer>Copyright © <?php echo date('Y') ?>. All rights reserved.</footer>
+<?php get_footer() ?>
