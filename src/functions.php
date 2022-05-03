00371e40cd02cc8fc81b04711795b86f77daafa9
Ng Wing Tat, David <i@ngwingt.at>
2022-05-04 05:10:08 +0800

    ✨ Add menu

--- src/functions.php
+++ src/functions.php
@@ -3,6 +3,15 @@
 // https://developer.wordpress.org/themes/basics/including-css-javascript/#stylesheets
 wp_enqueue_style('style', get_stylesheet_uri());
 
+// https://developer.wordpress.org/reference/functions/wp_enqueue_script/
+wp_enqueue_script(
+  'menu',
+  get_template_directory_uri() . '/assets/js/menu.js',
+  array('jquery'),
+  wp_get_theme()->get( 'Version' ),
+  true
+);
+
 // Add specific CSS class by filter.
 
 add_filter('body_class', function($classes) {
@@ -12,5 +21,25 @@ add_filter('body_class', function($classes) {
     'has-text-dark',
   ));
 });
+
+add_theme_support( 'menus' );
+
+function register_menus() {
+  register_nav_menus(
+    array(
+      'header-menu' => __('Header Menu'),
+    )
+  );
+}
+add_action('init', 'register_menus');
+
+function add_header_menu_classes($classes, $item, $args) {
+  if ('header-menu' === $args->theme_location) {
+    $classes[] = 'navbar-item';
+  }
+
+  return $classes;
+}
+add_filter('nav_menu_css_class', 'add_header_menu_classes', 10, 4);
 
 ?>
--- src/header.php
+++ src/header.php
@@ -9,12 +9,40 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <header class="hero-head section">
-	<nav class="navbar" role="navigation" aria-label="main navigation">
-		<div class="container is-flex is-justify-content-center">
-			<h1 class="title">
-				<a href="<?php echo site_url(); ?>"><?php echo get_bloginfo( 'name' ); ?></a>
-			</h1>
-		</div>
-	</nav>
+	<div class="container">
+		<nav class="navbar" role="navigation" aria-label="main navigation">
+			<div class="navbar-brand">
+				<a class="media is-align-items-center" href="<?php echo site_url(); ?>">
+					<div class="media-left">
+						<figure class="image is-48x48">
+							<img class="is-rounded" src="<?php echo get_template_directory_uri() . '/assets/images/logo.png'; ?>">
+						</figure>
+					</div>
+					<div class="media-content">
+						<h1 class="title"><?php echo get_bloginfo( 'name' ); ?></h1>
+					</div>
+				</a>
+				<a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+					<span aria-hidden="true"></span>
+					<span aria-hidden="true"></span>
+					<span aria-hidden="true"></span>
+				</a>
+			</div>
+
+			<div class="navbar-menu">
+					<?php
+					// https://developer.wordpress.org/reference/functions/wp_nav_menu/
+					wp_nav_menu(
+						array(
+							'theme_location' => 'header-menu',
+							'container' => '',
+							'items_wrap' => '<ul id="%1$s" class="%2$s navbar-end">%3$s</ul>',
+							'depth' => 1,
+						)
+					);
+					?>
+			</div>
+		</nav>
+	</div>
 </header>
 <main class="hero-body">
