c4a598348271a45deb4ed7f43a67537f0d16b84e
Ng Wing Tat, David <i@ngwingt.at>
2022-05-04 03:04:56 +0800

    ✨ Add the loop

--- src/index.php
+++ src/index.php
@@ -1,4 +1,19 @@
 <?php 
 get_header();
+
+// https://developer.wordpress.org/themes/basics/the-loop/#what-the-loop-can-display
+if (have_posts()) { ?>
+  <div><h1 class="title">Plog</h1></div>
+  <?php while (have_posts()) { ?>
+    <?php the_post(); ?>
+    <article>
+      <h1><?php the_title(); ?></h1>
+      <div><?php the_content(); ?></div>
+    </article>
+  <?php }
+} else {
+  _e('No more plog.', 'sadpugs');
+}
+
 get_footer();
 ?>
